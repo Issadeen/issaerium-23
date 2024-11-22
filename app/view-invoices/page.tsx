070d8fe383237d0cc83a073be914891f11d8c54b
Issadeen <75924084+Issadeen@users.noreply.github.com>
2024-11-22 15:31:53 +0000

    up3

--- app/view-invoices/page.tsx
+++ app/view-invoices/page.tsx
@@ -6,11 +6,24 @@ import { ArrowLeft, LogOut, Maximize, Minimize, Download } from 'lucide-react';
 import { Button } from "../components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../components/ui/table";
-import { ref, onValue } from 'firebase/database';
-import { onAuthStateChanged, signOut, User } from 'firebase/auth';
-import { auth, database } from '../firebaseConfig';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, onValue } from 'firebase/database';
+import { getAuth, onAuthStateChanged, signOut, User } from 'firebase/auth';
 import jsPDF from 'jspdf';
 import autoTable from 'jspdf-autotable';
+
+const firebaseApp = initializeApp({
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+});
+
+const database = getDatabase(firebaseApp);
+const auth = getAuth(firebaseApp);
 
 export default function ViewInvoices() {
     const [user, setUser] = useState<User | null>(null);
